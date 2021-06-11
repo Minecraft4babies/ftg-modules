2e7456886d7ec8c4e1307891ccbef0965e3d414f
Minecraft4babies <85728124+Minecraft4babies@users.noreply.github.com>
2021-06-11 09:54:25 +0300

    Add files via upload

---
+++ Zamena by @minecraft4babies.py
@@ -0,0 +1,18 @@
+#Модуль создан @minecraft4babies по приколу ради себя
+import requests
+from .. import loader, utils
+
+
+def register(cb):
+    cb(ZamenaMod())
+    
+class ZamenaMod(loader.Module):
+    """Замена с сайта ccte.nau.edu.ua"""
+    strings = {'name': 'Замены by @minecraft4babies'}
+    
+    async def zamenacmd(self, message):
+        """"Кидает замены картинкой.\nИспользование: .zamena <город>; ничего."""
+        await message.edit("Узнаем замены...")
+        zamenapng = requests.get(f"http://ccte.nau.edu.ua/images/Zameni.jpg").content
+        await message.client.send_file(message.to_id, zamenapng)
+        await message.delete()
---
+++ weather by @minecraft4babies.py
@@ -0,0 +1,27 @@
+import requests
+from .. import loader, utils
+
+
+def register(cb):
+    cb(WeatherMod())
+    
+class WeatherMod(loader.Module):
+    """Погода с сайта wttr.in"""
+    strings = {'name': 'Weather'}
+    
+    async def pwcmd(self, message):
+        """"Кидает погоду картинкой.\nИспользование: .pw <город>; ничего."""
+        args = utils.get_args_raw(message).replace(' ', '+')
+        args = utils.get_args_raw(message).replace('1', 'Вишгород')
+        await message.edit("Узнаем погоду...")
+        city = requests.get(f"https://wttr.in/{args if args != None else ''}.png?lang=uk").content
+        await message.client.send_file(message.to_id, city)
+        await message.delete()
+
+
+    async def awcmd(self, message):
+        """Кидает погоду ascii-артом.\nИспользование: .aw <город>; ничего."""
+        city = utils.get_args_raw(message)
+        await message.edit("Узнаем погоду...")
+        r = requests.get(f"https://wttr.in/{city if city != None else ''}?0?q?T&lang=ru")
+        await message.edit(f"<code>Город: {r.text}</code>")
