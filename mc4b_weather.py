01adc6f4166571a27f9bd00d42c501804cb105bc
Minecraft4babies <85728124+Minecraft4babies@users.noreply.github.com>
2022-02-01 21:58:04 +0200

    Update and rename weather_by_@minecraft4babies.py to mc4b_weather.py
    Урегулювання назви та спрощення коду.

--- mc4b_weather.py
+++ mc4b_weather.py
@@ -1,3 +1,5 @@
+#Minecraft4babies's module
+#For free use ;)
 import requests
 from .. import loader, utils
 
@@ -10,18 +12,17 @@ class WeatherMod(loader.Module):
     strings = {'name': 'Weather'}
     
     async def pwcmd(self, message):
-        """"Дає погоду картинкою.\nВикористання: .pw <місто>/нічого."""
+        """"Дає погоду картинкою.\nВикористання: <code>.pw</code> <місто>/нічого."""
         args = utils.get_args_raw(message).replace(' ', '+')
-        args = utils.get_args_raw(message).replace('1', 'Вишгород')
         await message.edit("Миколина погода працює...")
-        city = requests.get(f"https://wttr.in/{args if args != None else ''}.png?lang=uk").content
+        city = requests.get(f"https://wttr.in/{args if args != None else 'Вишгород'}.png?lang=uk").content
         await message.client.send_file(message.to_id, city)
         await message.delete()
 
 
     async def awcmd(self, message):
-        """Дає погоду ascii-артом.\nВикористання: .aw <місто>; нічого."""
+        """Дає погоду ascii-артом.\nВикористання: <code>.aw</code> <місто>; нічого."""
         city = utils.get_args_raw(message)
         await message.edit("Миколина погода працює...")
-        r = requests.get(f"https://wttr.in/{city if city != None else ''}?0?q?T&lang=uk")
+        r = requests.get(f"https://wttr.in/{city if city != None else 'Вишгород'}?0?q?T&lang=uk")
         await message.edit(f"<code>Місце: {r.text}</code>")
