dd26be399a351a59f208c97605cac0e2b79d9bb3
Minecraft4babies <85728124+Minecraft4babies@users.noreply.github.com>
2022-03-26 21:55:02 +0100

    Исправил аргсы при реплае

--- Youtube-Dl.py
+++ Youtube-Dl.py
@@ -45,6 +45,8 @@ class YtDlMod(loader.Module):
 
 async def riper(self, m, type):
 	reply = await m.get_reply_message()
+	reply = reply.replace("youtube.com/shorts", "youtu.be")
+	reply = reply.replace("?feature=share", "")
 	args = utils.get_args_raw(m)
 	args = args.replace("youtube.com/shorts","youtu.be")
 	args = args.replace("?feature=share", "")
