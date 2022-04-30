ba21f44f4834d9bbad2139d120656805e9420827
Minecraft4babies <85728124+Minecraft4babies@users.noreply.github.com>
2022-04-30 23:09:03 +0200

    Кринжанул

--- mc4b_minusrus.py
+++ mc4b_minusrus.py
@@ -60,7 +60,7 @@ class minusrus_mc4b1Mod(loader.Module):
 
     @loader.unrestricted
     async def deadruscmd(self, message):
-        """.deadrus <"snore" или ничего> чтобы получить случайных мёртвых солдатиков))\n"""
+        """.deadrus <"snore" или ничего> чтобы получить случайных мёртвых солдатиков))""
 
         await message.edit(self.strings['loading_deadrus'])
         reply = await message.get_reply_message()
