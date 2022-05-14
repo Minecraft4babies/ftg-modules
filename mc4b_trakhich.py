0fcbcfddd59ad88de694b6ac8587cd0ead2daa6c
Minecraft4babies <85728124+Minecraft4babies@users.noreply.github.com>
2022-05-14 22:32:10 +0200

    Довёл до ума-разума
    Просто косметика

--- mc4b_trakhich.py
+++ mc4b_trakhich.py
@@ -1,3 +1,11 @@
+"""
+    ____  __  __ _                            __ _   _  _   _           _     _
+   / __ \|  \/  (_)_ __   ___  ___ _ __ __ _ / _| |_| || | | |__   __ _| |__ (_) ___  ___
+  / / _` | |\/| | | '_ \ / _ \/ __| '__/ _` | |_| __| || |_| '_ \ / _` | '_ \| |/ _ \/ __|
+ | | (_| | |  | | | | | |  __/ (__| | | (_| |  _| |_|__   _| |_) | (_| | |_) | |  __/\__ \
+  \ \__,_|_|  |_|_|_| |_|\___|\___|_|  \__,_|_|  \__|  |_| |_.__/ \__,_|_.__/|_|\___||___/
+   \____/
+"""
 import io
 import requests
 import random
@@ -5,24 +13,27 @@ from .. import loader, utils
 
 
 def register(cb):
-    cb(trakhich_mc4b1Mod())
+    cb(pokemonorscp_mc4b1Mod())
 
 
-class trakhich_mc4b1Mod(loader.Module):
-    """Кого трахнешь?)"""
+class pokemonorscp_mc4b1Mod(loader.Module):
+    """Кого трахнешь?) Модуль выводит две картинки: случайного покемона и SCP-объект с таким же номером."""
 
-    strings = {'name': 'Trakhich'}
+    strings = {'name': 'PokemonOrSCP'}
 
     def __init__(self):
         self.name = self.strings['name']
         self._me = None
         self._ratelimit = []
 
-    async def fuckcmd(self, message):
-        """ .fuck"""
+    async def pokemonorscpcmd(self, message):
+        """Получить случайного покемона и SCP-объект
+
+
+        👨‍💻Made by: @Minecraft4babies_GFTG_Modules"""
 
         await message.edit("Собираем информацию...")
-        randint = (str(random.randint(1,899))).ljust(3, "0")
+        randint = (str(random.randint(1,898))).ljust(3, "0")
         reply = await message.get_reply_message()
 
 
@@ -31,11 +42,11 @@ class trakhich_mc4b1Mod(loader.Module):
         url = "https://webshot.deam.io/{}/?width=1000&height=1080?type=png"
         file = requests.get(url.format(scplink))
         file = io.BytesIO(file.content)
-        file.name = "scpfuck.png"
+        file.name = f"scp{randint}fuck.png"
         file.seek(0)
 
         pokemonpng = requests.get(f"https://assets.pokemon.com/assets/cms2/img/pokedex/full/{randint}.png").content
         await message.delete()
-        await message.client.send_file(message.chat_id, file=pokemonpng, caption="Трахнешь это...", reply_to=reply)
-        await message.client.send_message(message.chat_id, message="или, может...")
-        await message.client.send_file(message.chat_id, file, caption="это?)")
+        pokemon_msg = await message.client.send_file(message.chat_id, file=pokemonpng, caption=f"Трахнешь покемона под номером {randint}...", reply_to=reply)
+        or_message = await message.client.send_message(message.chat_id, message="или...", reply_to=pokemon_msg)
+        await message.client.send_file(message.chat_id, file, caption="Может, SCP-объект с этим номером?)", reply_to=or_message)
